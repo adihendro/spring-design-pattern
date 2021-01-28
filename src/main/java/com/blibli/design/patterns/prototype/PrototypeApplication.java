76e5ab19f6e323fd7e10af3b7113f14e73be37e3
adihendro <adi21hendro@gmail.com>
2021-01-28 13:05:13 +0700

    update example prototype

--- src/main/java/com/blibli/design/patterns/prototype/PrototypeApplication.java
+++ src/main/java/com/blibli/design/patterns/prototype/PrototypeApplication.java
@@ -25,7 +25,8 @@ public class PrototypeApplication {
 
     @SpringBootApplication
     public static class Configuration{
-        @Bean
+
+        @Bean("standardCustomer")
         @Scope("prototype")
         public Customer standardCustomer(){
             return Customer.builder()
@@ -33,13 +34,26 @@ public class PrototypeApplication {
                     .discount(10)
                     .build();
         }
+
+        @Bean("premiumCustomer")
+        @Scope("prototype")
+        public Customer premiumCustomer(){
+            return Customer.builder()
+                    .category("PREMIUM")
+                    .discount(50)
+                    .build();
+        }
     }
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Configuration.class);
-        Customer customerStandard1 = context.getBean(Customer.class);
-        Customer customerStandard2 = context.getBean(Customer.class);
-        Customer customerStandard3 = context.getBean(Customer.class);
+        Customer customerStandard1 = context.getBean("standardCustomer", Customer.class);
+        Customer customerStandard2 = context.getBean("standardCustomer", Customer.class);
+        Customer customerStandard3 = context.getBean("standardCustomer", Customer.class);
+
+        Customer premiumCustomer1 = context.getBean("premiumCustomer", Customer.class);
+        Customer premiumCustomer2 = context.getBean("premiumCustomer", Customer.class);
+        Customer premiumCustomer3 = context.getBean("premiumCustomer", Customer.class);
 
         System.out.println(customerStandard1 == customerStandard2);
         System.out.println(customerStandard2 == customerStandard3);
@@ -47,5 +61,22 @@ public class PrototypeApplication {
         System.out.println(customerStandard1);
         System.out.println(customerStandard2);
         System.out.println(customerStandard3);
+
+        System.out.println(premiumCustomer1);
+        System.out.println(premiumCustomer2);
+        System.out.println(premiumCustomer3);
+
+//    Customer customerStandard1 = Customer.builder().id("1").name("Eko").
+//        category("STANDARD").discount(10).build();
+//
+//    Customer customerStandard2 = new Customer();
+//    BeanUtils.copyProperties(customerStandard1, customerStandard2);
+//
+//    Customer customerStandard3 = Customer.builder().id("1").name("Eko").
+//        category(customerStandard1.getCategory()).discount(customerStandard1.getDiscount()).build();
+//
+//    Customer customerStandard4 = Customer.builder().id("1").name("Eko").
+//        category(customerStandard1.getCategory()).discount(customerStandard1.getDiscount()).build();
+
     }
 }
