b89772e4f91a7fa96a9bce5d4d36225ba565b203
adihendro <adi21hendro@gmail.com>
2021-01-28 10:12:30 +0700

    init project

---
+++ src/test/java/com/blibli/design/patterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blibli.design.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
